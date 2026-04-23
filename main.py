67bd4eac79f7bc4032cef9cd324a1151b2e9fa60
0xMattieu <87768964+0xMattieu@users.noreply.github.com>
2026-04-24 02:24:00 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:23:50.638Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-23T23:23:59.442Z
+Commit: Fix memory leak in event handlers
