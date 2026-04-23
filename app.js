3d87f74fcc2e0acb144a5bdcc13edbd3aec761a7
0xMattieu <87768964+0xMattieu@users.noreply.github.com>
2026-04-24 02:23:57 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:23:53.606Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-23T23:23:57.330Z
+Commit: Fix broken links in documentation
