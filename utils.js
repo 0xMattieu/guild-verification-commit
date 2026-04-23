9606b48970d7bd341f9a1b3b80d4876cc392c363
0xMattieu <87768964+0xMattieu@users.noreply.github.com>
2026-04-24 02:27:53 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:27:48.476Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-23T23:27:52.807Z
+Commit: Resolve timezone issues in date handling
