6ae38fe333a3a87e6c952a595e4f7281e6060d3f
0xMattieu <87768964+0xMattieu@users.noreply.github.com>
2026-04-24 02:23:54 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T23:23:34.506Z
-Commit: Implement user profile customization
+Timestamp: 2026-04-23T23:23:53.606Z
+Commit: Fix CSS styling bug on mobile devices
